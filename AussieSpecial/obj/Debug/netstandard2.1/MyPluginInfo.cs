baffd94aab61d828c26a4168082f5843e375d5ed
Little-Sparroh <dadn90e2mohti3mmkqb3@sent.com>
2026-08-26 03:40:10 +0000

    Delete AussieSpecial/obj directory

--- AussieSpecial/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace AussieSpecial
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "AussieSpecial";
-        public const string PLUGIN_NAME = "AussieSpecial";
-        public const string PLUGIN_VERSION = "0.1.0";
-    }
-}
