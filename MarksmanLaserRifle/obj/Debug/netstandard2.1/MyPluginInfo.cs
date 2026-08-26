13bf307c3c5030055becd9a59b553184120743e0
Little-Sparroh <dadn90e2mohti3mmkqb3@sent.com>
2026-08-26 03:40:53 +0000

    Delete MarksmanLaserRifle/obj directory

--- MarksmanLaserRifle/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MarksmanLaserRifle
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "MarksmanLaserRifle";
-        public const string PLUGIN_NAME = "MarksmanLaserRifle";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
--- MarksmanLaserRifle/obj/Release/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace DMLRRework
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "DMLRRework";
-        public const string PLUGIN_NAME = "DMLRRework";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
