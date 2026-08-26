715961c794800258228661401ab7a3f6e851a41a
Little-Sparroh <dadn90e2mohti3mmkqb3@sent.com>
2026-08-26 03:40:32 +0000

    Delete RapturesChosen/obj directory

--- RapturesChosen/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace RapturesChosen
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "RapturesChosen";
-        public const string PLUGIN_NAME = "RapturesChosen";
-        public const string PLUGIN_VERSION = "0.1.0";
-    }
-}
